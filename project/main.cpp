2fdc121928770c2075312290bca4c3ead4218e6a
Trey Bertram <treybertram06@gmail.com>
2024-11-11 15:58:59 -0800

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Trey Bertram on 2024-11-11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
